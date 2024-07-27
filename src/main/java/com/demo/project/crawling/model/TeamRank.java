13f2f3fabadb34d1eed573c9b8cf3714f407b102
EunJung Kim <dlqnfdl@gmail.com>
2024-07-27 22:32:20 +0900

    teamRank bug fix

--- src/main/java/com/demo/project/crawling/model/TeamRank.java
+++ src/main/java/com/demo/project/crawling/model/TeamRank.java
@@ -2,18 +2,13 @@ package com.demo.project.crawling.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.IdClass;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.util.Objects;
 
 @Entity
 @Setter
 @Getter
-@IdClass(TeamRankId.class)
 public class TeamRank {
-    @Id
     private int rank;
     @Id
     private String teamName;
@@ -24,17 +19,4 @@ public class TeamRank {
     private double winRate;
     private double winningMargin;
     private String continuity;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        TeamRank teamRank = (TeamRank) o;
-        return Objects.equals(rank ,teamRank.rank) && Objects.equals(teamName ,teamRank.teamName);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(rank, teamName);
-    }
 }
